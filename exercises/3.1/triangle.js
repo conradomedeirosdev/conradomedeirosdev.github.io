8ac17640f40707c23d1ce38802eba5a9efbc237c
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-09-23 19:31:46 -0300

    triangle adicionado usando condicionais

---
+++ exercises/3.1/triangle.js
@@ -0,0 +1,11 @@
+let angulo1 = 130;
+let angulo2 = 20;
+let angulo3 = 30;
+let soma = angulo1 + angulo2 + angulo3;
+if (soma == 180){
+    console.log(true);
+}else if (soma != 180){
+    console.log(false);
+}else {
+    console.log("Você não colocou um número");
+}
