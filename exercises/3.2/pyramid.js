0772e66b008b89f92e39b83d78875d8e68e642e4
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-09-26 15:26:59 -0300

    pyramid finalizado

--- exercises/3.2/pyramid.js
+++ exercises/3.2/pyramid.js
@@ -2,7 +2,7 @@ var n = 5;
 var a = "*";
 var b = 0;
 var c = " ";
-var d = 5;
+var d = n;
 for (i = 0; i < n; i++) {
-  console.log(c.repeat(--d) + a.repeat(++b));
+  console.log(c.repeat(--d) + a.repeat(b++) + a.repeat(b));
 }
--- exercises/3.2/triangle2.js
+++ exercises/3.2/triangle2.js
@@ -1,8 +1,8 @@
-var n = 5;
+var n = 10;
 var a = "*";
 var b = 0;
 var c = " ";
-var d = 5;
+var d = n;
 for (i = 0; i < n; i++) {
   console.log(c.repeat(--d) + a.repeat(++b));
 }
