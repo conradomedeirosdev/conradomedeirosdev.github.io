9e6591e93510a91994691d586ee8c0598158dfe4
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-09-26 18:19:47 -0300

    preime_number.js finalizado

---
+++ exercises/3.2/prime_number.js
@@ -0,0 +1,16 @@
+let n = 92;
+let primo;
+for (i = 2; i <= n; i++){
+    if(n == 2){
+        primo = true;
+        break;
+    }
+    else if (n % i == 0){
+        primo = false;
+        break;
+    }else{
+        primo = true;
+        break;
+    }
+}
+console.log(primo);
