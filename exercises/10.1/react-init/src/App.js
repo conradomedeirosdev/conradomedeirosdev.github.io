561694e2394eab04928c22d27f0d753603d47e86
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-11-13 18:02:53 -0300

    exercise 1

--- exercises/10.1/react-init/src/App.js
+++ exercises/10.1/react-init/src/App.js
@@ -3,21 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul className="list">
+        {arr.map(list => <li>{list}</li>)}
+        </ul>
       </header>
     </div>
   );
